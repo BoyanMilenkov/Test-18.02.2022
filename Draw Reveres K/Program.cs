d263ae7c05ae92ad900afd839e870e35476ac2dc
BoyanMilenkov <80035234+BoyanMilenkov@users.noreply.github.com>
2022-02-23 08:58:14 +0200

    Add files via upload

---
+++ Draw Reveres K/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Draw_Reveres_K
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v = int.Parse(Console.ReadLine());
+            int a;
+            Draw(v, a = 0);
+        }
+        private static void Draw(int v, int a)
+        {
+            if (v <= 0)
+            {
+                return;
+            }
+            Console.Write(new string(' ', a));
+            Console.WriteLine(new string('#', v));
+            Draw(v - 1, a + 1);
+            Console.Write(new string(' ', a));
+            Console.WriteLine(new string('*', v));
+            
+        }
+    }
+}
---
+++ Reverse Vector/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Reverse_Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bits = int.Parse(Console.ReadLine());
+            int[] vector = new int[bits];
+            Gen01(0, vector);
+        }
+        static void Gen01(int index, int[] vector)
+        {
+            if (index >= vector.Length)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+            }
+            else
+            {
+                for (int i = 0; i <= index; i++)
+                {
+                    vector[index] = i;
+                    Gen01 (index + 1, vector); 
+                }
+            }
+        }
+    }
+}
